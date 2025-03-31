7ca80357ce956254d41da254816bfee3195d2071
YoshVP <luis.vera.p@tecsup.edu.pe>
2025-03-31 16:10:40 -0500

    add Student model

--- src/main/java/com/tecsup/lab03/lab03_example/model/student.java
+++ src/main/java/com/tecsup/lab03/lab03_example/model/student.java
@@ -1,5 +1,14 @@
 package com.tecsup.lab03.lab03_example.model;
 
 public class student {
-
+	private String name;
+	private int age;
+	
+	public student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() { return name; }
+	public int getAge() {  return age; }
 }
